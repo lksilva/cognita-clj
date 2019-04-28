1352709d7b3870ef1f7f1bf9554e0ff7640556dd
Lucas <lucas_s.s@hotmail.com>
2019-04-28 12:10:32 -0300

    chore: add file of connection with database

---
+++ src/todo/database.clj
@@ -0,0 +1,13 @@
+(ns todo.database
+  (:require [korma.db :as korma]))
+
+(def db-connection-info 
+    (korma/mysql 
+        {:classname "com.mysql.jdbc.Driver"
+        :subprotocol "mysql"
+        :user "root"
+        :password "root"
+        :subname "//localhost:3306/todo"}))
+
+; set up korma
+(korma/defdb db db-connection-info)
